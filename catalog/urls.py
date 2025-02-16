abdb5cf0a189383f335882b8b0469eb289eb3d1a
Max Barulin <barulin.max@gmail.com>
2025-02-16 20:29:33 +0300

    init app_blogs

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from .views import CatalogListView, CatalogDetailView, CatalogTemplateView
 
 app_name = 'catalog'
 urlpatterns = [
-    path('home/', CatalogListView.as_view().show_home, name='show_home'),
+    path('home/', CatalogListView.as_view(), name='show_home'),
     path('', CatalogListView.as_view(), name='show_home'),
     path('contacts/', CatalogTemplateView.as_view(), name='contacts'),
     path('product/<int:pk>', CatalogDetailView.as_view(), name='product'),
